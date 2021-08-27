b42dde0d4ce22b404d4f2f6e61d468324d835c55
Joel McKinnon <mckinnonj@middleton.school.nz>
2021-08-27 21:42:13 +1200

    validation for question box, hiding the answer box and showing result on enter

--- Project/main.py
+++ Project/main.py
@@ -57,11 +57,14 @@ class GameScreen:
         helpButton(self.game_frame)
         # Question label
         self.question_label = Label(self.game_frame, text=self.question, fg="white", bg="grey")
-        self.answer_entry = Entry(self.game_frame, fg="black", width=10)
         self.question_label.grid(column=3, row=1, columnspan=5, pady=5)
+        self.answer_entry = Entry(self.game_frame, fg="black", width=10)
         self.answer_entry.grid(column=4, row=2, columnspan=3)
+        self.answer_label = Label(self.game_frame, text="empty", fg="white", bg="grey", width=10)
+        self.answer_label.grid(column=4, row=2, columnspan=3)
+        self.answer_label.grid_remove()
         # Button
-        self.enter = Button(self.game_frame, text="Enter", fg="Black", bg="White")
+        self.enter = Button(self.game_frame, text="Enter", fg="Black", bg="White", command=self.answer_question)
         self.enter.grid(column=4, row=4, columnspan=3, pady=(5, 0))
         # Add additional frames for formatting
         self.extra_frame1 = Frame(self.game_frame, width=200, height=405, bg="grey")
@@ -70,11 +73,34 @@ class GameScreen:
         self.extra_frame2.grid(column=9, row=0, rowspan=10)
         self.extra_frame3 = Frame(self.game_frame, height=205, bg="grey")
         self.extra_frame3.grid(column=10, row=8)
+        registration = self.game_frame.register(self.callback)
+        self.answer_entry.config(validate="key", validatecommand=(registration, '%P'))
         self.setQuestion()
 
     def setQuestion(self):
         self.question = Question(self.round_number)
         self.question_label.configure(text=self.question.to_string())
+
+    def answer_question(self):
+        answer = self.answer_entry.get()
+        self.answer_entry.delete(0, END)
+        self.answer_entry.grid_remove()
+        self.answer_label.grid()
+        if self.question.checks(answer): #true/correct
+            self.answer_label.configure(text="Correct!", fg="green")
+        else:
+            self.answer_label.configure(text="Incorrect!", fg="red")
+
+    def callback(self, input):
+        if input.isdigit():
+            return True
+        elif input.startswith("--"):
+            return False
+        elif input.startswith("-"):
+            return True
+        elif input == "":
+            return True
+        return False
 
 # help box popup
 def help_popup(window):
