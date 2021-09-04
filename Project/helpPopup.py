d020b05c8b608b42c45a8f406604269b11d21c08
McRules2004 <mckinnonoj@middleton.school.nz>
2021-09-04 14:25:52 +1200

    added help text

--- Project/helpPopup.py
+++ Project/helpPopup.py
@@ -5,5 +5,6 @@ from help import Help
 def help_popup(window):
     print("help2")
     get_help = Help(window)
-    get_help.help_text.configure(text="Help Text Here",
-                                 fg="white", font=("arial", "14"))
+    get_help.help_text.configure(text="-To save your results please    enter your name\n-Press start"
+                                      "\n-Enter result and press enter\n-Will tell u whether u were    correct"
+                                      "\n-Continue for the 10    questions", bg="Gray", fg="white", font=("arial", "14"))
